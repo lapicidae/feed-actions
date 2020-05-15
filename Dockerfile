0d7a25fca41305e5e6c7921ce6d6c691711ab591
lapicidae <andreas.hemmerle@gmail.com>
2020-05-16 00:32:23 +0200

    Initial Release

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM alpine:latest
+
+LABEL maintainer="lapicidae"
+
+ENV TZ="Europe/Berlin"
+
+# copy local files
+COPY root/ /
+
+RUN echo "**** install runtime packages ****" && \
+    echo "@testing http://nl.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories && \
+    apk add --no-cache --upgrade \
+      bash \
+      coreutils \
+      curl \
+      grep \
+      tzdata && \
+    apk add --no-cache rsstail@testing && \
+    echo "*********** set timezone *********" && \
+    ln -s /usr/share/zoneinfo/$TZ /etc/localtime && \
+    echo $TZ >/etc/timezone && \
+    echo "********* set permissions ********" && \
+    chmod 755 /usr/bin/docker-entrypoint.sh && \
+    echo "************ link files **********" && \
+    ln -s /usr/bin/docker-entrypoint.sh /docker-entrypoint.sh && \
+    echo "************ init cron ***********" && \
+    crontab /defaults/fa-cron && \
+    echo "************* cleanup ************" && \
+    rm -rf /var/cache/apk/*
+
+VOLUME ["/config"]
+
+ENTRYPOINT ["/docker-entrypoint.sh"]
---
+++ root/usr/bin/docker-entrypoint.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+cnf="/etc/mysql/conf.d/mysqlepgd.cnf"
+
+echo '
+--------------------------------------------------------------
+                   ____  ____  ____  ____
+                  ( ___)( ___)( ___)(  _ \
+                   )__)  )__)  )__)  )(_) )
+                  (__)  (____)(____)(____/
+           __    ___  ____  ____  _____  _  _  ___
+          /__\  / __)(_  _)(_  _)(  _  )( \( )/ __)
+         /(__)\( (__   )(   _)(_  )(_)(  )  ( \__ \
+        (__)(__)\___) (__) (____)(_____)(_)\_)(___/
+
+         https://github.com/lapicidae/feed-actions
+--------------------------------------------------------------'
+echo '
+  Please use the file "fa-cron" to adjust the scheduled time.
+
+--------------------------------------------------------------'
+echo "
+ Timezone:    $TZ
+
+--------------------------------------------------------------
+"
+
+# make folders
+if [ ! -d "/config" ]; then
+	mkdir -p /config
+fi
+
+# add fa-cron file
+if [ ! -e /config/fa-cron ]; then
+	cp /defaults/fa-cron /config/fa-cron
+fi
+
+# add config.cfg file
+if [ ! -e /config/config.cfg ]; then
+	cp /defaults/config.cfg /config/config.cfg
+fi
+
+# set timezone
+if [ -d "/usr/share/zoneinfo/$TZ" ]; then
+	CURRENT_TZ=$(readlink /etc/localtime)
+	if [[ ! $CURRENT_TZ =~ $TZ ]]; then
+		unlink /etc/localtime
+		ln -s /usr/share/zoneinfo/$TZ /etc/localtime
+		echo $TZ >/etc/timezone
+		echo "New timezone $TZ set!"
+	fi
+fi
+
+# set crontab
+crontab /config/fa-cron
+
+exec crond -f
